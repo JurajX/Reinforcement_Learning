c746c99d7f7442b2c7db542e0973b13773695bba
JurajX <jurajvrabel1@gmail.com>
2018-04-27 16:01:11 +0200

    adding possibility to render gym env inline (when useing a server)
    this requires to install also matplotlib and JSAnimation

--- Approximate_Solution_Methods/HelperFunctions_ASM.py
+++ Approximate_Solution_Methods/HelperFunctions_ASM.py
@@ -107,3 +107,58 @@ def visualizeAgent(env, agent):
         cum_reward = cum_reward + reward
         env.render()
     print('Return of this visualization was {0}.'.format(cum_reward))
+
+#-----  Visualising steps in iPython notebook
+
+import matplotlib.pyplot as plt
+from JSAnimation.IPython_display import display_animation
+from matplotlib import animation
+from IPython.display import display
+
+def display_frames_as_gif(frames):
+    """
+    Displays a list of frames as a gif, with controls
+    """
+    plt.figure(figsize=(frames[0].shape[1] / 72.0, frames[0].shape[0] / 72.0), dpi = 72)
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
+    display(display_animation(anim, default_mode='loop'))
+
+def visualizeVF_inline(env, actionVF, epsilon):
+    """
+    Args: gym environment, action-value function, epsilon parameter.
+    Renders the steps taken by passed value function.
+    """
+    frames = []
+    state = env.reset()
+    done = False
+    cum_reward = 0
+    while not done:
+        action = actionVF.epsGreedyPolicy(state, epsilon)
+        state, reward, done, _ = env.step(action)
+        cum_reward = cum_reward + reward
+        frames.append(env.render(mode='rgb_array'))
+    print('Return of this visualization was {0}.'.format(cum_reward))
+    display_frames_as_gif(frames)
+
+def visualizeAgent_inline(env, agent):
+    """
+    Args: gym environment, action-value function.
+    Renders the steps taken by the passed agent, the agent must have chooseAction member function.
+    """
+    frames = []
+    state = env.reset()
+    done = False
+    cum_reward = 0
+    while not done:
+        action = agent.chooseAction(state[np.newaxis, :])
+        state, reward, done, _ = env.step(action)
+        cum_reward = cum_reward + reward
+        frames.append(env.render(mode='rgb_array'))
+    print('Return of this visualization was {0}.'.format(cum_reward))
+    display_frames_as_gif(frames)
